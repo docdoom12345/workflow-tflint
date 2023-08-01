a5dc389fb1620571c67064e928f7cc15d1535df1
docdoom12345 <103731163+docdoom12345@users.noreply.github.com>
2023-08-01 16:51:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
-locals {
+/*locals {
   a = 40
+}*/
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.67.0"
+    }
+  }
+  required_version = '>1.1.0'
 }
 provider "azurerm" {
   # Configuration options
